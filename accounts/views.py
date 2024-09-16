c2854505087d95dfe437c5f68ea139a75a4d32af
emmzak <emmanuelzakaryan@gmail.com>
2024-09-16 09:28:57 +0200

    handle perms

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ from django.core.serializers import serialize
 
 
 def is_admin(user):
-    return user.is_superuser or user.groups.filter(name='admin').exists()
+    return user.is_superuser or user.groups.filter(name='Admin').exists()
 
 # @login_required
 # @user_passes_test(is_admin)
