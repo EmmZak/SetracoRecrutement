ad7f819d31e5f5b79c95cc132d111ba4d778feb1
emmzak <emmanuelzakaryan@gmail.com>
2024-11-04 20:19:14 +0100

    test csrf trusted

--- SetracoRecrutement/settings/prod.py
+++ SetracoRecrutement/settings/prod.py
@@ -42,7 +42,7 @@ SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
+#SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 
 INSTALLED_APPS = [
     'dbbackup',  # django-dbbackup
